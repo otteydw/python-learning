c4141f7577e926c11c97cdf125672254a01286cc
Dan Ottey <dottey@boomi.com>
2019-03-09 11:10:04 -0500

    problem9

---
+++ snakify/lesson1/problem9.py
@@ -0,0 +1,17 @@
+# https://snakify.org/en/lessons/print_input_numbers/problems/
+
+# A school decided to replace the desks in three classrooms. Each desk sits two students. Given the number of students in each class, print the smallest
+#  possible number of desks that can be purchased.
+# The program should read three integers: the number of students in each of the three classes, a, b and c respectively.
+
+# In the first test there are three groups. The first group has 20 students and thus needs 10 desks. The second group has 21 students, so they can get by
+# with no fewer than 11 desks. 11 desks is also enough for the third group of 22 students. So we need 32 desks in total.
+
+# round() was rounding 10.5 to 10...  so I chose to use ciel instead.
+import math
+
+a = float(input('Number of students in class a: '))
+b = float(input('Number of students in class b: '))
+c = float(input('Number of students in class c: '))
+
+print(math.ceil(a/2) + math.ceil(b/2) + math.ceil(c/2))
