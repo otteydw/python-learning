aa15b5dc9426c901a866a98cf1993fab94210aa8
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-09 09:51:30 -0500

    problem2

---
+++ snakify/lesson1/problem2.py
@@ -0,0 +1,7 @@
+# https://snakify.org/en/lessons/print_input_numbers/problems/
+
+# Write a program that greets the person by printing the word "Hi" and the name of the person. See the examples below.
+
+name = input('What\'s your name? ')
+
+print('Hi ' + name)
