4c88a3c3c077163eb7e2f2583f3c5de432c33b93
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-09 10:00:08 -0500

    problem3

---
+++ snakify/lesson1/problem3.py
@@ -0,0 +1,8 @@
+# https://snakify.org/en/lessons/print_input_numbers/problems/
+
+# Write a program that takes a number and print its square.
+
+number = int(input('Enter a number: '))
+square = number**2
+
+print('The square is ' + str(square))
