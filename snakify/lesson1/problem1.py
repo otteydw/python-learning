2f88da79874b7d78d14fc3eb0056f92ae87474f8
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-09 09:45:36 -0500

    Initial something something

---
+++ snakify/lesson1/problem1.py
@@ -0,0 +1,9 @@
+# https://snakify.org/en/lessons/print_input_numbers/problems/aplusbplusc/
+
+# Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+
+a = int(input('First number: '))
+b = int(input('Second number: '))
+c = int(input('Third number: '))
+
+print(a + b + c)
