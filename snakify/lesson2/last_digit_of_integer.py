eebc77ddd743cb86a96224b9b71cd22f1ba517d9
Dan Ottey <dottey@boomi.com>
2019-03-22 11:34:28 -0400

    last digit of integer

---
+++ snakify/lesson2/last_digit_of_integer.py
@@ -0,0 +1,9 @@
+# https://snakify.org/en/lessons/integer_float_numbers/problems/
+
+# Given an integer number, print its last digit.
+
+import math
+
+a = str(input('Enter an integer: '))
+
+print(a[-1])
