27351fe33cb6f82ecf06baffa6b8e14684b9ece8
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-22 15:46:37 -0400

    first lesson3 problems

---
+++ snakify/lesson3/minimum.py
@@ -0,0 +1,11 @@
+# https://snakify.org/en/lessons/if_then_else_conditions/problems/
+
+# Given two integers, print the smaller value.
+
+a = int(input('Integer 1: '))
+b = int(input('Integer 2: '))
+
+if a <= b:
+    print(a)
+else:
+    print(b)
---
+++ snakify/lesson3/minimum_of_three.py
@@ -0,0 +1,14 @@
+# https://snakify.org/en/lessons/if_then_else_conditions/problems/
+
+# Given three integers, print the smallest value.
+
+a = int(input('Integer 1: '))
+b = int(input('Integer 2: '))
+c = int(input('Integer 2: '))
+
+if a <= b and a <= c:
+    print(a)
+elif b <= a and b <= c:
+    print(b)
+else:
+    print(c)
