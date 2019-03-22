ff1d13d0b7937b09e4d5f1b3ee35390b1a7e4a45
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-22 13:18:54 -0400

    remove commented out math

--- snakify/lesson2/total_cost.py
+++ snakify/lesson2/total_cost.py
@@ -4,6 +4,8 @@
 # The program should print two numbers: the number of hours(between 0 and 23) and the number of minutes(between 0 and 59).
 # For example, if N = 150, then 150 minutes have passed since midnight - i.e. now is 2: 30 am. So the program should print 2 30.
 
+# import math
+
 n = int(input('Number of minutes that have passed since midnight: '))
 
 hours = int(n / 60)
