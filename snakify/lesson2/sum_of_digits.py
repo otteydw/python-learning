2708d298d9d4de59261c37587ac5b91130982c06
Daniel W. Ottey <dan@bluefrogs.us>
2019-03-22 12:39:14 -0400

    Additional lesson 2 scripts.

---
+++ snakify/lesson2/sum_of_digits.py
@@ -0,0 +1,9 @@
+# https://snakify.org/en/lessons/integer_float_numbers/problems/
+
+# Given a three-digit number. Find the sum of its digits.
+
+a = str(input('Enter an integer: '))
+
+sum = int(a[0]) + int(a[1]) + int(a[2])
+
+print(str(sum))
---
+++ snakify/lesson2/tens_digit.py
@@ -0,0 +1,7 @@
+# https://snakify.org/en/lessons/integer_float_numbers/problems/
+
+# Given an integer. Print its tens digit.
+
+a = '0' + str(input('Enter an integer: '))
+
+print(a[-2])
