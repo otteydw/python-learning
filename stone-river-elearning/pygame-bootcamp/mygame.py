1cf0e8c39ac416d33045cb99573ea17a5b63c446
Daniel W. Ottey <dan@bluefrogs.us>
2019-04-22 15:56:02 -0400

    Basic skeleton of pygame.  Debug print the events.

--- stone-river-elearning/pygame-bootcamp/mygame.py
+++ stone-river-elearning/pygame-bootcamp/mygame.py
@@ -9,3 +9,13 @@ height = 700
 screenDim = (width, height)
 
 screen = pygame.display.set_mode(screenDim)
+
+finished = False
+while finished == False:
+
+    # Process all the events
+    for event in pygame.event.get():
+        # Do the things
+        print(event)
+
+    pygame.display.flip()   # Update the display
