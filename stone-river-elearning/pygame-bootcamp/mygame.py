9331d4f25f745bcb50f24d4a4dd329b628a07c87
Dan Ottey <dottey@boomi.com>
2019-04-25 19:55:23 -0400

    Move the goal to screen center

--- stone-river-elearning/pygame-bootcamp/mygame.py
+++ stone-river-elearning/pygame-bootcamp/mygame.py
@@ -23,10 +23,8 @@ pygame.display.set_caption('My First Game')
 grassImage = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/grass.png').convert()
 grassImage = pygame.transform.scale(grassImage, screenDim)
-screen.blit(grassImage, (0, 0))
 
 rescale = 3
-
 player = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/characterBody.png').convert_alpha()
 playerWidth = player.get_rect().width
@@ -34,7 +32,6 @@ playerHeight = player.get_rect().height
 player = pygame.transform.scale(
     player, (playerWidth*rescale, playerHeight*rescale))
 player = pygame.transform.rotate(player, 90)
-# screen.blit(player, (0, 0))
 
 foot = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/characterFoot.png').convert_alpha()
@@ -43,7 +40,6 @@ footHeight = foot.get_rect().height
 foot = pygame.transform.scale(
     foot, (footWidth*rescale, footHeight*rescale))
 foot = pygame.transform.rotate(foot, 90)
-# screen.blit(foot, (0, 0))
 
 rescaleBall = 2
 ball = pygame.image.load(
@@ -52,9 +48,7 @@ ballWidth = ball.get_rect().width
 ballHeight = ball.get_rect().height
 player = pygame.transform.scale(
     ball, (ballWidth*rescaleBall, ballHeight*rescaleBall))
-# screen.blit(ball, (0, 0))
 
-# goalRescale = 2
 goalLeft = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/goalLeft.png').convert_alpha()
 goalLeft = pygame.transform.scale(goalLeft, (250, 270))
@@ -63,7 +57,6 @@ goalLeftHeight = goalLeft.get_rect().height
 adjust = 12
 goalLeft = cropSurface(goalLeftWidth/2+adjust, goalLeftHeight/2 +
                        adjust, goalLeftWidth/2-adjust, goalLeftHeight/2-adjust, goalLeft)
-screen.blit(goalLeft, (0, 0,))
 
 goalMiddle = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/goalMiddle.png').convert_alpha()
@@ -72,18 +65,25 @@ goalMiddleWidth = goalMiddle.get_rect().width
 goalMiddleHeight = goalMiddle.get_rect().height
 goalMiddle = cropSurface(
     goalMiddleWidth, goalMiddleHeight/2+adjust, 0, goalMiddleHeight/2-adjust, goalMiddle)
-screen.blit(goalMiddle, (goalLeft.get_rect().width, 0,))
 
 goalRight = pygame.image.load(
     '/home/dottey/git/python-learning/stone-river-elearning/pygame-bootcamp/images/goalRight.png').convert_alpha()
 goalRight = pygame.transform.scale(goalRight, (250, 270))
 goalRightWidth = goalRight.get_rect().width
 goalRightHeight = goalRight.get_rect().height
-# goalRight = cropSurface(
-#     goalRightWidth, goalRightHeight/2+adjust, 0, goalRightHeight/2-adjust, goalRight)
 goalRight = cropSurface(
     goalRightWidth/2+adjust, goalRightHeight/2+adjust, 0, goalRightHeight/2-adjust, goalRight)
-screen.blit(goalRight, (goalLeft.get_rect().width +
+
+screen.blit(grassImage, (0, 0))
+# screen.blit(player, (0, 0))
+# screen.blit(foot, (0, 0))
+# screen.blit(ball, (0, 0))
+
+goalStart = (width - goalLeft.get_rect().width -
+             goalMiddle.get_rect().width - goalRight.get_rect().width)/2
+screen.blit(goalLeft, (goalStart, 0,))
+screen.blit(goalMiddle, (goalStart + goalLeft.get_rect().width, 0,))
+screen.blit(goalRight, (goalStart + goalLeft.get_rect().width +
                         goalMiddle.get_rect().width, 0,))
 
 finished = False
