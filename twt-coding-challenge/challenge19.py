6ea38929cae0191340378406f31d2f2a84841735
Dan Ottey <dottey@boomi.com>
2020-09-18 13:28:18 -0400

    Got one solution working for challenge19

--- twt-coding-challenge/challenge19.py
+++ twt-coding-challenge/challenge19.py
@@ -3,48 +3,102 @@
 
 def solution(n, encrypted_message):
 
-    def get_adjacent_prime_products(encrypted_message):
-        all = encrypted_message
-        all.sort()
-        all = list(dict.fromkeys(all))
-        return all
+    # big_list = encrypted_message
 
-    def get_previous_prime(current_prime, previous_prime_product):
-        previous_prime = previous_prime_product / current_prime
-        return previous_prime
+    def find_factors(maxval, number):
+        for x in range(3, maxval):
+            for y in range(x, maxval):
+                if x * y == number:
+                    return x, y
 
-    def is_prime(n):
-        factors=0
+    # current_factor = find_factors(n, encrypted_message[0])[0]
+    alphabet_factors_all = []
 
-        for x in range(1, n):
-            if n % x == 0:
-                factors+=1
-                if factors > 1:
-                    return False
-        return True
+    # while len(big_list) > 0:
+    #     print(current_factor)
+    #     alphabet_factors.append(current_factor)
+    #     big_list.pop(0)
+    #     current_factor = big_list[0] / current_factor
 
-    def find_z(n):
-        for z in range(n, 1, -1):
-            print(z)
-            if is_prime(z):
-                return z
+    # print(alphabet_factors)
 
-    z = find_z(n)
+    for product in encrypted_message:
+        factors = find_factors(n, product)
+        # print(factors)
+        for factor in factors:
+            alphabet_factors_all.append(factor)
 
-    ordered_prime_products = get_adjacent_prime_products(encrypted_message)
-    # print(len(ordered_prime_products))
-    ordered_primes = [z]
-    current_prime = z
-    while len(ordered_prime_products) > 0:
-        print('Current prime: ' + str(current_prime))
-        current_product = ordered_prime_products[-1]
-        previous_prime = get_previous_prime(current_prime, current_product)
-        ordered_primes.append(previous_prime)
-        current_prime = previous_prime
-        ordered_prime_products.remove(current_product)
-        print()
+    # print(alphabet_factors_all)
+    alphabet_factors = alphabet_factors_all.copy()
+    alphabet_factors.sort()
+    alphabet_factors = list(dict.fromkeys(alphabet_factors))
+    # print()
+    # print(alphabet_factors)
+    # print(len(alphabet_factors))
 
-    return ordered_primes
+    letters = []
+    for num in range(ord('A'), ord('Z')+1):
+        letters.append(chr(num))
+
+    # alphabet = dict(zip(letters, alphabet_factors))
+    alphabet = dict(zip(alphabet_factors, letters))
+
+    # print(alphabet)
+
+    decryped_message_with_dupes = []
+    for number in alphabet_factors_all:
+        decryped_message_with_dupes.append(alphabet[number])
+
+    decrypted_message = decryped_message_with_dupes[::2]
+    decrypted_message.append(decryped_message_with_dupes[-1])
+    # print(''.join(str(x) for x in decrypted_message))
+    # return decrypted_message
+    return ''.join(str(x) for x in decrypted_message)
+
+    # def get_adjacent_prime_products(encrypted_message):
+    #     all = encrypted_message
+    #     all.sort()
+    #     all = list(dict.fromkeys(all))
+    #     return all
+
+    # def get_previous_prime(current_prime, previous_prime_product):
+    #     previous_prime = previous_prime_product / current_prime
+    #     return previous_prime
+
+    # def is_prime(n):
+    #     factors=0
+
+    #     for x in range(1, n):
+    #         if n % x == 0:
+    #             factors+=1
+    #             if factors > 1:
+    #                 return False
+    #     return True
+
+    # def find_z(n):
+    #     for z in range(n, 1, -1):
+    #         print(z)
+    #         if is_prime(z):
+    #             return z
+
+    # z = find_z(n)
+
+    # ordered_prime_products = get_adjacent_prime_products(encrypted_message)
+    # # print(len(ordered_prime_products))
+    # ordered_primes = [z]
+    # current_prime = z
+    # while len(ordered_prime_products) > 0:
+    #     print('Current prime: ' + str(current_prime))
+    #     current_product = ordered_prime_products[-1]
+    #     previous_prime = get_previous_prime(current_prime, current_product)
+    #     ordered_primes.append(previous_prime)
+    #     current_prime = previous_prime
+    #     ordered_prime_products.remove(current_product)
+    #     print()
+
+    # return ordered_primes
+
+
 
 
 
