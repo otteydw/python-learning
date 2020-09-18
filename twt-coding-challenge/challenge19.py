edbd64819b5eaaa056dd3207f033467903bc4fbd
Daniel W. Ottey <dan@bluefrogs.us>
2020-09-18 11:49:27 -0400

    Challenge 19 is still confusing.  Stopping here.

--- twt-coding-challenge/challenge19.py
+++ twt-coding-challenge/challenge19.py
@@ -1,7 +1,7 @@
 # >>> print(solution(9565, [73153709, 73410599, 73564913, 73770857, 73925603, 74028791, 74235407, 74390621, 74442383, 74563189, 74718727, 74908961, 75099547, 75220913, 75325037, 75429209, 75533477, 75620407, 75742193, 75864091, 75968647, 76125589, 76282747, 76370117, 76457527])
 # ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
-def solution(z, encrypted_message):
+def solution(n, encrypted_message):
 
     def get_adjacent_prime_products(encrypted_message):
         all = encrypted_message
@@ -12,6 +12,24 @@ def solution(z, encrypted_message):
     def get_previous_prime(current_prime, previous_prime_product):
         previous_prime = previous_prime_product / current_prime
         return previous_prime
+
+    def is_prime(n):
+        factors=0
+
+        for x in range(1, n):
+            if n % x == 0:
+                factors+=1
+                if factors > 1:
+                    return False
+        return True
+
+    def find_z(n):
+        for z in range(n, 1, -1):
+            print(z)
+            if is_prime(z):
+                return z
+
+    z = find_z(n)
 
     ordered_prime_products = get_adjacent_prime_products(encrypted_message)
     # print(len(ordered_prime_products))
@@ -45,5 +63,5 @@ def solution(z, encrypted_message):
 # is_prime(5)
 
 
-# print(solution(9565, [1, 2, 3, 5, 4, 5, 3, 1]))
+# print(solution(10, [1, 2, 3, 5, 4, 5, 3, 1]))
 print(solution(9565, [73153709, 73410599, 73564913, 73770857, 73925603, 74028791, 74235407, 74390621, 74442383, 74563189, 74718727, 74908961, 75099547, 75220913, 75325037, 75429209, 75533477, 75620407, 75742193, 75864091, 75968647, 76125589, 76282747, 76370117, 76457527]))
